52df20df9fbc7fdde4ea280c29793269d9cb1245
raviprakashgiri <raviprakashgiri@gmail.com>
2017-01-06 00:57:51 -0500

    now starting

---
+++ reverse_words.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+/*
+** Without considering starting space or more than one space
+**
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void reverse_words(char* arr)
+{
+	
+}
+
+
+int main(int argc, char const *argv[])
+{
+	
+	char arr[] = "Ravi Prakash Giri";
+	cout<< "before reversal:"<<arr<<endl;
+	reverse_words(arr);
+	cout<< "after reversal:"<<arr<<endl;
+	
+	return 0;
+}
