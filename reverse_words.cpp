b6c84c371ebb27fdbd68ed443e5e3a9ccb1d3575
raviprakashgiri <raviprakashgiri@gmail.com>
2017-01-06 01:06:11 -0500

    now starting

--- reverse_words.cpp
+++ reverse_words.cpp
@@ -8,10 +8,38 @@
 
 using namespace std;
 
+void reverse(char* begin, char* end)
+{
+	char temp;
+	while(end > begin)
+	{
+		temp = *begin;
+		*begin = *end;
+		*end = temp;
+		begin++;
+		end--;
+	}
+}
 
 void reverse_words(char* arr)
 {
-	
+	char* arr_copy = arr;
+	char* word_start = arr;
+
+	while(*arr_copy) 
+	{
+		arr_copy++;
+		if (*arr_copy == '\0')
+		{
+			reverse(word_start, arr_copy - 1);
+		}
+		else if (*arr_copy == ' ')
+		{
+			reverse(word_start, arr_copy - 1);
+			word_start = arr_copy + 1;
+		}
+	}
+	reverse(arr, arr_copy - 1);
 }
 
 
