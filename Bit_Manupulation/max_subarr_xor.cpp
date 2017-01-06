cf9a738b0196544d28b9266ded058848be5c3516
raviprakashgiri <raviprakashgiri@gmail.com>
2017-01-06 15:22:42 -0500

    order n^2 algo... :bad:

--- Bit_Manupulation/max_subarr_xor.cpp
+++ Bit_Manupulation/max_subarr_xor.cpp
@@ -1,17 +1,37 @@
 // #include <iostream>
+/*
+** O(n^2)
+**
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-void calculate_xor(int* arr)
+int calculate_xor(int* arr, int arr_size)
 {
 	
+	//cout<<arr_size<<endl;
+	int max_xor = INT_MIN;
+
+	for (int i = 0; i < arr_size; ++i)
+	{
+		int xor_init = 0;
+		for (int j = i; j < arr_size; ++j)
+			{
+				xor_init ^= arr[j];
+				max_xor = max(max_xor, xor_init);
+			}	
+	}
+	return max_xor;
 }
 
 
 int main(int argc, char const *argv[])
 {
-	int arr[] = {1, 2, 3, 5, 8};
+	int arr[] = {1, 2, 12, 8};
+	int arr_size = sizeof(arr) / sizeof(arr[0]);
+	int result = calculate_xor(arr, arr_size);
+	cout<<"max xor is:"<<result<<endl;
 
 	return 0;
 }
