175c8d0ab0ed0e2b52fb2f0dabea9c1edbe9f447
raviprakashgiri <raviprakashgiri@gmail.com>
2017-01-06 01:28:00 -0500

    started... :sunglass

--- Bit_Manupulation/max_subarr_xor.cpp
+++ Bit_Manupulation/max_subarr_xor.cpp
@@ -1,11 +1,17 @@
 // #include <iostream>
 #include <bits/stdc++.h>
 
-
 using namespace std;
+
+void calculate_xor(int* arr)
+{
+	
+}
+
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	int arr[] = {1, 2, 3, 5, 8};
+
 	return 0;
 }
