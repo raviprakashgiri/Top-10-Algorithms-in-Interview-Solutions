6a2a53bbc125f22f6fe94dd99a11f285948311ab
raviprakashgiri <raviprakashgiri@gmail.com>
2017-01-07 00:45:58 -0500

    bit diff done :sunglasses:

--- Bit_Manupulation/bit_diff_among_pairs.cpp
+++ Bit_Manupulation/bit_diff_among_pairs.cpp
@@ -1,9 +1,31 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 
-using namsespace std;
+/*
+** For 32-bit of integer
+**
+*/
+
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	
+	int arr[] = {1, 3, 5};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	int result = 0;
+
+	for (int i = 0; i < 32; ++i)
+	{
+		int count = 0;
+		for (int j = 0; j < size; ++j)
+		{
+			if(arr[j] & 1 << i)
+				count++;
+		}
+		result += count * (size-count) * 2;
+	}
+
+	cout<<result<<endl;
+
 	return 0;
 }
