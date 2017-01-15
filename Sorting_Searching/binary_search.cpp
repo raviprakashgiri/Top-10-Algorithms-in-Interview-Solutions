640398324c7a495b2fd1b45db4e5e11267204c99
raviprakashgiri <raviprakashgiri@gmail.com>
2017-01-15 02:25:09 -0500

    just to be sure

--- Sorting_Searching/binary_search.cpp
+++ Sorting_Searching/binary_search.cpp
@@ -4,22 +4,31 @@ using namespace std;
 
 bool binary_search(int* arr, int begin, int end, int element)
 {
-	if (begin < end)
-	{
-		if (element == arr[(begin+end)/2])
-		{
-			return true;
-		}
-		else if (element < arr[(begin+end)/2])
-		{
-			return binary_search(arr, begin, (begin+end)/2, element);
-		}
-		else
-			return binary_search(arr, ((begin+end)/2) +1, end, element);
-	}
-	else
-		return false;
+	// if (begin < end)
+	// {
+	// 	if (element == arr[(begin+end)/2])
+	// 	{
+	// 		return true;
+	// 	}
+	// 	else if (element < arr[(begin+end)/2])
+	// 	{
+	// 		return binary_search(arr, begin, (begin+end)/2, element);
+	// 	}
+	// 	else
+	// 		return binary_search(arr, ((begin+end)/2) +1, end, element);
+	// }
+	// else
+	// 	return false;
 	
+	if (end < begin)
+		return false;
+	int mid = (begin + end) / 2;
+	if (element == arr[mid])
+		return true;
+	else if (element < arr[mid])
+		return binary_search(arr, begin, mid -1, element);
+	return binary_search(arr, mid + 1, end, element);
+
 }
 
 int main(int argc, char const *argv[])
@@ -28,7 +37,7 @@ int main(int argc, char const *argv[])
 	int size = sizeof(arr)/sizeof(arr[0]);
 	sort(arr, arr + size);
 	int element = 1;
-	binary_search(arr, 0, size, element)? cout<<"Found"<<endl:cout<<"Not found"<<endl;
+	binary_search(arr, 0, size -1, element)? cout<<"Found"<<endl:cout<<"Not found"<<endl;
 
 	return 0;
 }
