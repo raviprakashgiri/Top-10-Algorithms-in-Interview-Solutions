1226eef03e539f6c61c9ddb865c5583a61d5e889
raviprakashgiri <raviprakashgiri@gmail.com>
2017-01-14 22:41:55 -0500

    return added

--- Sorting_Searching/binary_search.cpp
+++ Sorting_Searching/binary_search.cpp
@@ -12,10 +12,10 @@ bool binary_search(int* arr, int begin, int end, int element)
 		}
 		else if (element < arr[(begin+end)/2])
 		{
-			binary_search(arr, begin, (begin+end)/2, element);
+			return binary_search(arr, begin, (begin+end)/2, element);
 		}
 		else
-			binary_search(arr, ((begin+end)/2) +1, end, element);
+			return binary_search(arr, ((begin+end)/2) +1, end, element);
 	}
 	else
 		return false;
@@ -27,7 +27,7 @@ int main(int argc, char const *argv[])
 	int arr[] = {2, 4, 5, 23, 434, 12, 45, 65 , 34};
 	int size = sizeof(arr)/sizeof(arr[0]);
 	sort(arr, arr + size);
-	int element = 34;
+	int element = 1;
 	binary_search(arr, 0, size, element)? cout<<"Found"<<endl:cout<<"Not found"<<endl;
 
 	return 0;
